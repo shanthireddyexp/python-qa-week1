47eb03325c1f21774ec68f15d78f1da32b4f45fe
shanthireddyexp <shanthireddyexp@gmail.com>
2026-05-12 06:57:09 +0530

    added file

--- pytestPrograms/pytest_basic_scenarios.py
+++ pytestPrograms/pytest_basic_scenarios.py
@@ -7,24 +7,3 @@ def test_login():
 def test_logout():
     print("This is logout test")
     
-def setup_module():
-    print("Setup for the entire module")
-    
-def teardown_module():
-    print("Teardown for the entire module")
-    
-def setup_function():
-    print("Setup for each test function")
-    
-def teardown_function():
-    print("Teardown for each test function")
-    
-@pytest.fixture(scope="function")
-def sample_fixture():
-    print("Setup for sample fixture")
-    yield
-    print("Teardown for sample fixture")
-    
-@pytest.mark.usefixtures("sample_fixture")
-def test_sample():
-    print("This is a sample test using fixture")    
