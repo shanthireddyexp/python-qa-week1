cc289915e37f49b4642478f545c0b05fb110537a
shanthireddyexp <shanthireddyexp@gmail.com>
2026-05-13 08:54:21 +0530

    basic Programs

---
+++ basicPrograms/basicMathematicalPrograms.py
@@ -0,0 +1,16 @@
+"""Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation"""
+
+firstNum = int(input("Enter the first number: "))
+secondNum = int(input("Enter the second number: "))
+
+print("Addition:", firstNum + secondNum)
+print("Subtraction:",firstNum - secondNum)
+print("Multiplication:", firstNum * secondNum)
+print("Division:",firstNum/secondNum)
---
+++ basicPrograms/createPersonalizedGreeting.py
@@ -0,0 +1,9 @@
+"""Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name."""
+
+firstName = input("Enter your First name: ")
+lastName = input("Enter your Last Name: ")
+
+print(f"Hello, {firstName} {lastName} welcome to the python program.")
